c9b3d7beea208fa961826c53ebee8387f4bdfc36
kyung-hak <hak0504@gmail.com>
2018-07-20 09:27:19 +0900

    메인메뉴추가
    잘될까?ㅋ

---
+++ main.py
@@ -0,0 +1,27 @@
+'''
+Created on 2018. 7. 19.
+
+@author: 6조, 박중규, 민경학 , 박용훈
+'''
+
+if __name__ == '__main__':
+    print('''
+    ============================================================
+    1. print all buildings
+    2. print all performances
+    3. print all audiences
+    4. insert a new building
+    5. remove a new building
+    6. insert a new performance
+    7. remove a performance
+    8. insert a new audience
+    9. remove an audience 
+    10. assign a performance to a building
+    11. book a performance
+    12. print all performances which assigned at a building
+    13. print all audiences who booked for a performance
+    14. print ticket booking status of a performance
+    15. exit
+    16. reset database
+    ============================================================
+    ''')
