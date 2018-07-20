e92f64c6a2435a54aaf98153584d30eb3e24c7ce
soluse <soluse@gmail.com>
2018-07-20 13:15:31 +0900

    Class Test

--- main.py
+++ main.py
@@ -3,19 +3,20 @@ Created on 2018. 7. 19.
 
 @author: 6조, 박중규, 민경학 , 박용훈
 '''
-
-if __name__ == '__main__':
-    print('''
+class ConcertManagement:
+    
+    def show_menu(self):
+        print('''
     ============================================================
-    1. print all buildings
-    2. print all performances
-    3. print all audiences
-    4. insert a new building
-    5. remove a new building
-    6. insert a new performance
-    7. remove a performance
-    8. insert a new audience
-    9. remove an audience 
+     1. print all buildings
+     2. print all performances
+     3. print all audiences
+     4. insert a new building
+     5. remove a new building
+     6. insert a new performance
+     7. remove a performance
+     8. insert a new audience
+     9. remove an audience 
     10. assign a performance to a building
     11. book a performance
     12. print all performances which assigned at a building
@@ -25,3 +26,22 @@ if __name__ == '__main__':
     16. reset database
     ============================================================
     ''')
+     
+    def print_msg(self, type, msg):
+        msg = msg_format = "[{TYPE}] {MSG}".format(TYPE=type,MSG=msg)
+        print(msg)
+
+if __name__ == '__main__':
+    isRun = True
+    while isRun:
+        cm = ConcertManagement()
+        cm.show_menu()
+        sel=-1
+        try:
+            sel = int(input("Select Menu#: ")) 
+        except ValueError as e:
+            cm.print_msg("ERROR", "숫자만 입력해주세요")
+            input("Press any key to restart...")
+        if sel == 15: isRun=False    
+            
+            
